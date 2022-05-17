a69b8de7ff7b6034cef5096b3bdfa93dd036d7b9
arunkb2795 <arunkb27@gmail.com>
2022-05-17 15:25:53 +0530

    useReducer update

--- src/App.js
+++ src/App.js
@@ -1,16 +1,30 @@
-import React, { useState } from 'react';
+import React, { useReducer } from 'react';
 import './style.css';
 
+const initialSatate = 0;
+
+const reducer = (state, action) => {
+  switch (action) {
+    case 'increment':
+      return state + 1;
+    case 'decrement':
+      return state - 1;
+    case 'reset':
+      return initialSatate;
+    default:
+      return state;
+  }
+};
 export default function App() {
-  const [count, setCount] = useState(0);
+  const [count, dispatch] = useReducer(reducer, initialSatate);
   console.log('Rerendiring...');
   return (
     <div>
       <h1>Counter</h1>
       <p>Counter : {count}</p>
-      <button onClick={() => setCount((c) => c + 1)}>Increment</button>
-      <button onClick={() => setCount((c) => c - 1)}>Decrement</button>
-      <button onClick={() => setCount(0)}>Reset</button>
+      <button onClick={() => dispatch('increment')}>Increment</button>
+      <button onClick={() => dispatch('decrement')}>Decrement</button>
+      <button onClick={() => dispatch('reset')}>Reset</button>
     </div>
   );
 }
