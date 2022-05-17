3fe117a124b94719b1466906f71eab48f3507979
arunkb2795 <arunkb27@gmail.com>
2022-05-17 14:36:58 +0530

    Rerendering of useState

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
-import React from "react";
-import "./style.css";
+import React, { useState } from 'react';
+import './style.css';
 
 export default function App() {
+  const [count, setCount] = useState(0);
+  console.log('Rerendiring...');
   return (
     <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
+      <h1>Counter</h1>
+      <p>Counter : {count}</p>
+      <button onClick={() => setCount((c) => c + 1)}>Increment</button>
+      <button onClick={() => setCount((c) => c - 1)}>Decrement</button>
+      <button onClick={() => setCount(0)}>Reset</button>
     </div>
   );
 }
